de1bade6619f119a2f059f015f71dde4d07d2b2f
Haram Bae <hisfedev@gmail.com>
2020-03-03 17:01:05 +0900

    :wrench: Apply story router

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 import withGlobalStyle from './decorators';
 
 addDecorator(withGlobalStyle);
+addDecorator(StoryRouter());
