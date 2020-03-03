81eb640a94a381c72d1248895f247f63ab7f2348
Haram Bae <hisfedev@gmail.com>
2020-03-03 15:55:29 +0900

    :wrench: Enable absolute path using react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+/* config-overrides.js */
+
+const path = require('path');
+
+module.exports = function override(config) {
+  config.resolve.modules = [
+    ...(config.resolve.modules || []),
+    path.resolve(__dirname, 'src'),
+  ];
+
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
+    '@components': 'components',
+    '@pages': 'pages',
+    '@hooks': 'hooks',
+    '@styles': 'styles',
+    '@utils': 'utils',
+  };
+  return config;
+};
