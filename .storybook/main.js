e9f68cff3e29bfd3a0d9e03732e55c918926c5dc
Haram Bae <hisfedev@gmail.com>
2020-03-03 16:08:31 +0900

    :wrench: Apply storybook for absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
@@ -5,4 +7,20 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+  webpackFinal: (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, '../src'),
+    ];
+  
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@components': 'components',
+      '@pages': 'pages',
+      '@hooks': 'hooks',
+      '@styles': 'styles',
+      '@utils': 'utils',
+    };
+    return config;
+  }
 };
