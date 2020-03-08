16f65401911c98965b763ab7ccae6182e61e16c3
Haram Bae <hisfedev@gmail.com>
2020-03-08 17:51:22 +0900

    :sparkles: Add header for laptop

---
+++ src/components/Shared/HeaderLaptop.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import styled from 'styled-components';
+import { IoMdHeartEmpty, IoIosSearch } from 'react-icons/io';
+import { FaRegCompass } from 'react-icons/fa';
+import profile from '@images/profile.jpg';
+import { CircleImage, mainGrey } from '@styles/variables';
+import LayoutWrapper from '@styles/layout';
+import LogoIcon from './LogoIcon';
+import LogoText from './LogoText';
+
+const Header = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  background-color: white;
+  border-bottom: 1px solid #bbbbbb;
+  padding: 0 1rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 64px;
+`;
+
+const StyledLogoIcon = styled(LogoIcon)`
+  margin-right: 1rem;
+`;
+
+const Logo = styled.div`
+  display: flex;
+`;
+
+const UL = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 2rem;
+  width: 8rem;
+`;
+
+const Search = styled.div`
+  display: flex;
+  border: 1px solid #bbbbbb;
+  border-radius: 3px;
+  padding: 0.5rem 0.7rem;
+  background-color: #fafafa;
+  width: 300px;
+`;
+
+const SearchIcon = styled(IoIosSearch)`
+  font-size: 1.2rem;
+  margin-right: 0.3rem;
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  background: transparent;
+`;
+
+const HeaderLaptop = () => {
+  return (
+    <Header>
+      <LayoutWrapper>
+        <Container>
+          <Logo>
+            <StyledLogoIcon size="2rem" />
+            <LogoText size="2rem" />
+          </Logo>
+          <Search>
+            <SearchIcon />
+            <SearchInput type="text" placeholder="검색" />
+          </Search>
+          <nav>
+            <UL>
+              <li>
+                <IoMdHeartEmpty />
+              </li>
+              <li>
+                <FaRegCompass />
+              </li>
+              <li>
+                <CircleImage
+                  profileSize="2rem"
+                  src={profile}
+                  alt="프로필 사진"
+                />
+              </li>
+            </UL>
+          </nav>
+        </Container>
+      </LayoutWrapper>
+    </Header>
+  );
+};
+
+export default HeaderLaptop;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import HeaderMobile from '@components/Shared/HeaderMobile';
+import HeaderLaptop from '@components/Shared/HeaderLaptop';
 import { HomePost } from '@components/Home/index';
 import NavMobile from '@components/Shared/NavMobile';
 import profile from '@images/profile.jpg';
+import { device } from '@styles/variables';
 import Main from './Home.style';
 
 const Home = () => {
+  const isLaptop = useMediaQuery({ query: device.Laptops });
+
   return (
     <>
-      <HeaderMobile />
+      {isLaptop ? <HeaderLaptop /> : <HeaderMobile />}
       <Main>
         <HomePost />
       </Main>
-      <NavMobile path={profile} />
+      {!isLaptop && <NavMobile path={profile} />}
     </>
   );
 };
