1cc9f51be536366f42f05aa94647ae27fe407522
Haram Bae <hisfedev@gmail.com>
2020-03-17 15:12:06 +0900

    :art: Apply template

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
-import HeaderMobile from '@components/Shared/HeaderMobile';
-import HeaderLaptop from '@components/Shared/HeaderLaptop';
+import PageTemplate from '@components/Shared/PageTemplate';
 import { HomePost } from '@components/Home/index';
-import NavMobile from '@components/Shared/NavMobile';
-import profile from '@images/profile.jpg';
-import device from '@styles/device';
-import Main from './Home.style';
 
 const Home = () => {
-  const isLaptop = useMediaQuery({ query: device.Laptops });
-
   return (
-    <>
-      {isLaptop ? <HeaderLaptop /> : <HeaderMobile />}
-      <Main>
-        <HomePost />
-      </Main>
-      {!isLaptop && <NavMobile path={profile} />}
-    </>
+    <PageTemplate>
+      <HomePost />
+    </PageTemplate>
   );
 };
 
